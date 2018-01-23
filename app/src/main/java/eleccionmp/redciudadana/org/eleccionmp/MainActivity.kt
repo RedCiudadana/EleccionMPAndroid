e55f57b2a4a46e2f73992fbf16ffa67efcffabcf
Javier Pérez <javier.alejandro92@gmail.com>
2018-01-23 09:51:19 -0600

    added drawer layout example to application

--- app/src/main/java/eleccionmp/redciudadana/org/eleccionmp/MainActivity.kt
+++ app/src/main/java/eleccionmp/redciudadana/org/eleccionmp/MainActivity.kt
@@ -1,12 +1,18 @@
 package eleccionmp.redciudadana.org.eleccionmp
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import eleccionmp.redciudadana.org.eleccionmp.http.Api
 import eleccionmp.redciudadana.org.eleccionmp.http.Profile
 import eleccionmp.redciudadana.org.eleccionmp.views.mainmenu.MainMenuFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,11 +28,35 @@ interface MainView {
 private const val TAG: String = "MainActivity"
 
 class MainActivity : AppCompatActivity(), MainView {
-    private val api: Api = Api();
+    private val api: Api = Api()
+    private var mDrawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        mDrawerToggle = object: ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            override fun onDrawerClosed(drawerView: View?) {
+                super.onDrawerClosed(drawerView)
+            }
+
+            override fun onDrawerOpened(drawerView: View?) {
+                super.onDrawerOpened(drawerView)
+
+            }
+        }
+        drawer_layout.addDrawerListener(mDrawerToggle as ActionBarDrawerToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        // API call test
         val callResponse = api.api.getProfiles()
         callResponse.enqueue(object: Callback<List<Profile>> {
             override fun onFailure(call: Call<List<Profile>>?, t: Throwable?) {
@@ -44,6 +74,23 @@ class MainActivity : AppCompatActivity(), MainView {
         if (savedInstanceState == null) {
             showMainMenu()
         }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        mDrawerToggle?.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        mDrawerToggle?.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (mDrawerToggle?.onOptionsItemSelected(item)!!) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun showMainMenu() {
