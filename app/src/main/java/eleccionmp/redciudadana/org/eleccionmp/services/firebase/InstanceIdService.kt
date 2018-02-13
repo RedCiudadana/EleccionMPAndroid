79f14cb540e18f7320bd27516e5ffa1691d52f92
Javier Pérez <javier.alejandro92@gmail.com>
2018-02-12 21:21:30 -0600

    added logger for device

--- app/src/main/java/eleccionmp/redciudadana/org/eleccionmp/services/firebase/InstanceIdService.kt
+++ app/src/main/java/eleccionmp/redciudadana/org/eleccionmp/services/firebase/InstanceIdService.kt
@@ -1,9 +1,16 @@
 package eleccionmp.redciudadana.org.eleccionmp.services.firebase
 
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
 
 /**
  * Created by javier on 2/6/18.
  */
 
-class InstanceIdService: FirebaseInstanceIdService()
+class InstanceIdService: FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+        val token = FirebaseInstanceId.getInstance().token
+        print("Firebase token: ${token}")
+    }
+}
